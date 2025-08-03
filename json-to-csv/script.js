0db63dd653cc4797f10f95b57b0b1729c8ea6cd5
his984 <his984@gmail.com>
2025-08-04 00:18:26 +0200

    csv to json

--- json-to-csv/script.js
+++ json-to-csv/script.js
@@ -5,30 +5,21 @@ const toJsonBtn = document.getElementById("to-json-btn");
 const toCsvBtn = document.getElementById("to-csv-btn");
 const clearBtn = document.getElementById("clear-btn");
 const messageContainer = document.getElementById("message-container");
-
-// toJsonBtn.addEventListener("click", function (event) {
-//   event.preventDefault();
-
-// });
-const csvFile = `name,email,age,country
-Loy,labramin0@mapquest.com,8,Russia
-
-Stanislaw,slissenden1@amazonaws.com,18,Israel
-Chrisse,cjeannel2@techcrunch.com,6,France`;
-
-const lines = csvFile.split("\n");
-const [headerLine, ...contentLines] = lines;
-const headerLineArray = headerLine.split(",");
-
-const finalJsonArray = [];
-contentLines.forEach((line) => {
-  if (line.trim() === "") return;
-  const obj = {};
-  const values = line.split(",");
-  for (let i = 0; i < headerLineArray.length; i++) {
-    obj[headerLineArray[i]] = values[i];
-  }
-  finalJsonArray.push(obj);
+toJsonBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const csv = csvTextarea.value;
+  const lines = csv.split("\n");
+  const [headerLine, ...contentLines] = lines;
+  const headerLineArray = headerLine.split(",");
+  const finalJsonArray = [];
+  contentLines.forEach((line) => {
+    if (line.trim() === "") return;
+    const obj = {};
+    const values = line.split(",");
+    for (let i = 0; i < headerLineArray.length; i++) {
+      obj[headerLineArray[i]] = values[i];
+    }
+    finalJsonArray.push(obj);
+  });
+  jsonTextarea.textContent = JSON.stringify(finalJsonArray, null, 2);
 });
-
-// console.log(finalJsonArray);
